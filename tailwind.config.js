2745cb2c4ab15b5525373c986a3d456c04e79d34
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-04-01 22:14:57 -0300

    feat: added colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,28 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
     content: [
         "./pages/**/*.{js,ts,jsx,tsx}",
         "./components/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        ...colors,
+        extend: {
+            colors: {
+                primary: {
+                    DEFAULT: "#6BCEBB",
+                },
+                secundary: {
+                    DEFAULT: "#B3B3B3",
+                },
+                red: {
+                    DEFAULT: "#DD8DAF",
+                },
+                green: {
+                    DEFAULT: "#8DDDB2",
+                },
+            },
+        },
     },
     plugins: [],
 };
