cb906d6299e053272e5d772af076ef20caefde7b
Arkadina <61208486+Arkadina@users.noreply.github.com>
2023-04-02 20:23:16 -0300

    feat: new table style

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,30 @@ import Layout from "@/components/Layout";
 const Index = () => {
     return (
         <Layout>
-            <p>Index page</p>
+            <div className="flex justify-center w-full mt-20">
+                <table className="w-[80%]">
+                    <thead className=" text-primary tracking-widest text-left font-light ">
+                        <tr className="mb-10">
+                            <th className="">#</th>
+                            <th>USER ID</th>
+                            <th>EMAIL</th>
+                            <th>CREATED_AT</th>
+                            <th>STATUS</th>
+                            <th>ACTIONS</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr className="text-secundary text-[16px] h-[40px]  ">
+                            <td>1</td>
+                            <td>3464323532</td>
+                            <td>test@gmaill.com</td>
+                            <td>2121</td>
+                            <td>2121</td>
+                            <td>Editar Excluir</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </Layout>
     );
 };
